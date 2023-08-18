f3295a7ddf08b7620aec31c7fef2a72dc46b1d6a
Willem Aandewiel <Willem@Aandewiel.nl>
2023-08-18 09:57:23 +0200

    Added reading battVoltage

--- src/SmInrushBooster.cpp
+++ src/SmInrushBooster.cpp
@@ -32,31 +32,82 @@
 ** ATMEL ATTINY85
 **                        +--\/--+
 **             RESET PB5 1|      |8 VCC
-**    -->[AnalogIn]  PB3 2|      |7 PB2  --> HSSW1
-**              [x]  PB4 3|      |6 PB1  --> HSSW2
-**                   GND 4|      |5 PB0  --> HSSW3 
+**    -->[AnalogIn]  PB3 2|      |7 PB2  --> HSSW1_YEL
+**  <-- mySerial(Tx) PB4 3|      |6 PB1  --> HSSW2_BLU
+**                   GND 4|      |5 PB0  --> HSSW1_RED 
 **                        +------+
 **
 */
 
 #include <arduino.h>
+#ifdef ARDUINO_AVR_UNO
+  #define mySerial  Serial
+#else
+  #include <SoftwareSerial.h>
+  SoftwareSerial mySerial(-1,4);  //rx, tx
+#endif 
 
-#define HSSW1   0
-#define HSSW2   1
-#define HSSW3   2
+#define HSSW1_RED   0
+#define HSSW2_BLU   1
+#define HSSW1_YEL   2
+
+float       battV;
+int16_t     prevBattV10;
+uint32_t    loopCount = 0;
+
+//----------------------------------------------------------------
+float readAnalogVoltage(int pin) 
+{
+  // Set the analog reference to 1.1 volts.
+  //analogReference(INTERNAL);
+  ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits
+  ADCSRA |= bit (ADPS0) | bit (ADPS1) | bit (ADPS2);   // 128
+  //ADMUX  =  bit (REFS0) | bit (REFS1);  // Internal 1.1V reference
+
+  uint16_t avarage = 0;
+  uint16_t analogValue;
+
+  for (int i = 0; i < 15; i++)
+  {
+    // Read the analog value from the specified pin.
+    analogValue = analogRead(pin);
+    avarage += analogValue; 
+    delay(10);
+  }
+  analogValue = avarage / 15;
+  /*
+  mySerial.print("analogValue [");
+  mySerial.print(analogValue);
+  mySerial.print("] ");
+  */
+  // Calculate the voltage from the analog value.
+  //int16_t voltageI10 = (int)(analogValue * 5 / 1024.0); 
+  float voltage = (analogValue * 5.0) / 1024.0;
+
+  return voltage;
+
+} //  readAnalogVoltage()
+
 
 //----------------------------------------------------------------
 void setup() 
 {
-  digitalWrite(HSSW1, LOW);
-  digitalWrite(HSSW2, LOW);
-  digitalWrite(HSSW3, LOW);
-  pinMode(HSSW1, OUTPUT);
-  digitalWrite(HSSW1, LOW);
-  pinMode(HSSW2, OUTPUT);
-  digitalWrite(HSSW2, LOW);
-  pinMode(HSSW3, OUTPUT);
-  digitalWrite(HSSW3, LOW);
+  mySerial.begin(9600);
+  delay(500);
+  mySerial.flush();
+  mySerial.println("\r\nAnd than it begins ..\r\n");
+
+  digitalWrite(HSSW1_RED, LOW);
+  digitalWrite(HSSW2_BLU, LOW);
+  digitalWrite(HSSW1_YEL, LOW);
+  pinMode(HSSW1_RED, OUTPUT);
+  digitalWrite(HSSW1_RED, LOW);
+  pinMode(HSSW2_BLU, OUTPUT);
+  digitalWrite(HSSW2_BLU, LOW);
+  pinMode(HSSW1_YEL, OUTPUT);
+  digitalWrite(HSSW1_YEL, LOW);
+
+  loopCount = 0;
 
 } // setup()
 
@@ -65,11 +116,41 @@ void setup()
 void loop() 
 {
   delay(1000);
-  digitalWrite(HSSW1, !digitalRead(HSSW1));
-  delay(1000);
-  digitalWrite(HSSW2, digitalRead(HSSW1));
-  delay(1000);
-  digitalWrite(HSSW3, digitalRead(HSSW1));
+  digitalWrite(HSSW1_RED, !digitalRead(HSSW1_RED));
+  //digitalWrite(HSSW2_BLU, LOW);
+  //digitalWrite(HSSW1_YEL, LOW);
+  
+  battV = readAnalogVoltage(3);
+  int16_t battV10 = battV * 100;
+  battV = battV10 / 100.0;
+  loopCount++;
+
+  if ( ((loopCount%60) == 0) || (prevBattV10 != (int)(battV*10)) )
+  //if ( (loopCount%10) == 0) 
+  {
+    prevBattV10 = (int)battV*10;
+    mySerial.print("battVoltage is [");
+    mySerial.print(battV);
+    mySerial.print("]  ");
+
+    if (battV > 3.9)
+    {
+      digitalWrite(HSSW2_BLU, HIGH);
+      mySerial.println("Switch booster to 'ON' ..");
+    }
+    else if (battV > 3.6)
+    {
+      digitalWrite(HSSW1_YEL, HIGH);
+      digitalWrite(HSSW2_BLU, LOW);
+      mySerial.println("almost there ..");
+    }
+    else 
+    {
+      digitalWrite(HSSW2_BLU, LOW);
+      digitalWrite(HSSW1_YEL, LOW);
+      mySerial.println();
+    }
+  }
 
 } // loop()
 
